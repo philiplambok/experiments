e5e7e811009d04dabac1c0652750eab139a681c4
Philip Lambok <philiplambok71@gmail.com>
2022-07-22 21:01:08 +0700

    Full feature in check box

--- app/javascript/controllers/tables_controller.js
+++ app/javascript/controllers/tables_controller.js
@@ -2,11 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tables"
 export default class extends Controller {
-  static targets = ['admins']
+  static targets = ['items', 'masterCheck']
 
   toggleAll(event){
-    this.adminsTargets.forEach((el) => {
+    this.itemsTargets.forEach((el) => {
       el.checked = event.target.checked
     })
+  }
+  sync(_event) {
+    const checkedItems = this.itemsTargets.filter((el) => {
+      return el.checked == true
+    })
+    if(checkedItems.length == this.itemsTargets.length){
+      this.masterCheckTarget.checked = true
+    } else {
+      this.masterCheckTarget.checked = false
+    }
   }
 }
