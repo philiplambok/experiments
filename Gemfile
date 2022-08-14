b58e84d87c17e5691f5c0bdbe54541b1c624e83e
Philip Lambok <philiplambok71@gmail.com>
2022-08-14 16:26:06 +0700

    Use postgres

--- Gemfile
+++ Gemfile
@@ -11,8 +11,8 @@ gem "sprockets-rails"
 
 gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 
-# Use sqlite3 as the database for Active Record
-gem "sqlite3", "~> 1.4"
+# Use pg as the database for Active Record
+gem "pg", "~> 1.1"
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_13_093701) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -32,8 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_13_093701) do
   end
 
   create_table "product_bundles", force: :cascade do |t|
-    t.integer "product_id", null: false
-    t.integer "bundle_id", null: false
+    t.bigint "product_id", null: false
+    t.bigint "bundle_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["bundle_id"], name: "index_product_bundles_on_bundle_id"
