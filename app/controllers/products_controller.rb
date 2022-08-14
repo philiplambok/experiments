17d82888071e928cae40a4efe8f49209e99511dd
Philip Lambok <philiplambok71@gmail.com>
2022-08-14 12:16:42 +0700

    Working for the feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,16 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def update
+    product = Product.find params[:id]
+    product.update!(product_params)
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :amount)
+  end
 end
